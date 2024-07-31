51abe3ba15b49d2aa080b75276c72ef24635fa41
Prakash Rajesh Singh <101583807+singhprakash14@users.noreply.github.com>
2024-07-31 23:53:47 +0530

    Update tumsum.js

--- tumsum.js
+++ tumsum.js
@@ -1,4 +1,13 @@
+Two Integer Sum
 
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+
+
+    
     twoSum(nums, target) {
        const bag = {};
        for( let i=0; i<nums.length; i++){
